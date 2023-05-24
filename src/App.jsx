13f02aed2c35a6f664f7337887b2743ffa10d3f5
WHQ\11109310 <meowen0228@gmail.com>
2023-05-24 17:48:07 +0800

    update router

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import './i18n/i18n'
 
-import TimerHome from './pages/TimerHome'
-import Timer from './pages/Timer'
+import { Timer, TimerHome, TimerHistory } from './pages/index'
 
 function App() {
   return (
     <Routes>
       <Route path="/timer" element={<TimerHome />}>
         <Route index element={<Timer />} />
-        <Route path="history" element={<Timer />} />
+        <Route path="history" element={<TimerHistory />} />
         <Route path="template" element={<Timer />} />
         <Route path="style" element={<Timer />} />
       </Route>
---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+import Timer from "./Timer";
+import TimerHome from "./TimerHome";
+import TimerHistory from "./TimerHistory";
+
+export {
+  Timer,
+  TimerHome,
+  TimerHistory
+}
