611dec7b18c2142a8741255f5d975de2dbbca87d
WHQ\11109310 <meowen0228@gmail.com>
2023-05-24 15:34:02 +0800

    update timer home

---
+++ src/pages/TimerHome.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { useNavigate, Outlet } from 'react-router-dom'
+import { useState } from 'react'
+import { Button, Divider, Drawer, Menu, Typography } from 'antd'
+import {
+  FieldTimeOutlined,
+  InboxOutlined,
+  AppstoreAddOutlined,
+  SkinOutlined,
+  MenuOutlined,
+} from '@ant-design/icons'
+import './TimerHome.scss'
+
+const { Title } = Typography
+
+function TimerHome() {
+  const { t, i18n } = useTranslation()
+  const navigate = useNavigate();
+  const [open, setOpen] = useState(false)
+
+  const changeLanguageHandler = (lang = 'tw') => {
+    i18n.changeLanguage(lang)
+  }
+
+  const getItem = (key, label, icon, path) => {
+    return {
+      key,
+      icon,
+      label,
+      path,
+    }
+  }
+
+  const menuList = [
+    getItem(1, t('Timer'), <FieldTimeOutlined />, '/timer'),
+    getItem(2, t('History'), <InboxOutlined />, '/timer/history'),
+    getItem(3, t('Template'), <AppstoreAddOutlined />, '/timer/template'),
+    getItem(4, t('Style'), <SkinOutlined />, '/timer/style'),
+  ]
+
+  const showDrawer = () => {
+    setOpen(true)
+  }
+
+  const closeDrawer = () => {
+    setOpen(false)
+  }
+
+  const menuItemOnClick = (e) => {
+    closeDrawer()
+    navigate(e.item.props.path, { replace: true });
+  };
+
+  return (
+    <div className="timer_home">
+      <Button type="text" onClick={showDrawer} icon={<MenuOutlined />} />
+      <Drawer
+        placement="left"
+        closable={false}
+        onClose={closeDrawer}
+        open={open}
+        width={'60%'}
+      >
+        <Title level={4}>MENU</Title>
+        <Divider />
+        <Menu defaultSelectedKeys={['1']} mode="inline" items={menuList} onClick={menuItemOnClick} />
+        <Button onClick={() => changeLanguageHandler()}>lang: tw</Button>
+        <Button onClick={() => changeLanguageHandler('en')}>lang: en</Button>
+      </Drawer>
+      <Outlet />
+    </div>
+  )
+}
+
+export default TimerHome
